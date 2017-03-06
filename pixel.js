960be19e2a3021194f90e270c2d08d9ce587f601
ErikaNewland <erika.s.newland@gmail.com>
2017-03-06 10:55:03 -0500

    Created files for ssketchpad project

---
+++ pixel.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+
+//calculating initial grid spects
+var oneRow = 16
+var gridNumber = oneRow * oneRow
+var gridWidth = Math.floor(480/oneRow);
+
+//Creating initial grid
+for (i=1;i<=gridNumber;i++){
+$("#container").append("<div class=grid></div>");
+}
+
+
+//Size the initial grid
+$(".grid").css("width",gridWidth);
+$(".grid").css("height",gridWidth);
+
+//"Sketch" behavior
+$(".grid").on("mouseover",function(){
+  $(this).css("background-color","black");
+});
+
+//Clear button resets the grid
+$("button").on("click",function(){
+   //determine the grid size based on user input
+    oneRow = prompt("How many squares per row?");
+    gridNumber = oneRow * oneRow
+   //empty the container of the previous grid
+   $("#container").empty();
+   //Create the new grid
+      for (i=1;i<=gridNumber;i++){
+      $("#container").append("<div class=grid></div>");
+      }
+    //Calculate the new gride spects
+    gridWidth = Math.floor(480/oneRow);
+    //Size the new grid
+    $(".grid").css("width",gridWidth);
+    $(".grid").css("height",gridWidth);
+    //"Sketch" behavior
+    $(".grid").on("mouseover",function(){
+    $(this).css("background-color","black");
+  });
+
+
+  });
+
+
+
+
+
+
+});
